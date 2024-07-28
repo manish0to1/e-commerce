8a58d3a44a980f8fb9bb97c5250b57991e1a3e0e
Manish Nayak <nayak10softworld@gmail.com>
2024-07-28 19:40:12 +0530

    adding footer buttons text in footer

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -10,145 +10,160 @@ const Footer = () => {
         sx={{ bgcolor: "black", color: "white", py: 3 }}
       >
         <Grid item xs={12} sm={6} md={3}>
-          <Typography className="pb-5 " variant="h6">
+          <Typography className="pb-5" variant="h6" gutterBottom>
             Company
           </Typography>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
               About
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
               Blog
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
               Press
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Job's
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Jobs
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Contact
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Investers
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Partners
             </Button>
           </div>
         </Grid>
 
         <Grid item xs={12} sm={6} md={3}>
-          <Typography className="pb-5 " variant="h6">
-            Company
+          <Typography className="pb-5" variant="h6" gutterBottom>
+            Solutions
           </Typography>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              About
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Marketing
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Blog
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Analytics
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Press
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Commerce
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Job's
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Insights
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Contact
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Investers
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Support
             </Button>
           </div>
         </Grid>
-        
+
         <Grid item xs={12} sm={6} md={3}>
-          <Typography className="pb-5 " variant="h6">
-            Company
+          <Typography className="pb-5" variant="h6" gutterBottom>
+            Documentation
           </Typography>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              About
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Guidelines
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Blog
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Press
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Job's
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Contact
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Investers
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              API Status
             </Button>
           </div>
         </Grid>
-        
+
         <Grid item xs={12} sm={6} md={3}>
-          <Typography className="pb-5 " variant="h6">
-            Company
+          <Typography className="pb-5" variant="h6" gutterBottom>
+            Legal
           </Typography>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              About
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Claim
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Blog
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Privacy
             </Button>
           </div>
           <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Press
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Job's
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Contact
-            </Button>
-          </div>
-          <div>
-            <Button className="pb-5" variant="h6" gutterButtom>
-              Investers
+            <Button
+              className="pb-5"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              Terms & Conditions
             </Button>
           </div>
         </Grid>
