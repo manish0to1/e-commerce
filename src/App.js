d30d4b5ffba2d3c0eddd27c2626c8d66f4dd8d8c
Manish Nayak <nayak10softworld@gmail.com>
2024-07-29 22:37:59 +0530

    again trying to fix the navbar issue as before

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Footer from "./customer/components/Footer/Footer";
-import Navigation from "./customer/components/Navigation/Navigation";
+import Navigation from "./customer/components/Navigation/Navigation.jsx";
 import HomePage from "./customer/Pages/HomePage/HomePage";
 
 function App() {
