e6605daee57147a5492052bc89fec4482c1ef65a
Manish Nayak <nayak10softworld@gmail.com>
2024-07-30 12:40:25 +0530

    navigation issue resolved. Now the hidden images are shown when button clicked

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -171,7 +171,7 @@ const Footer = () => {
 
         <Grid className="pt-20" item xs={12}>
           <Typography variant="body2" component="p" align="center">
-            &copy; 2024 My Company. All rights reserved.
+            &copy; 2024 | All rights reserved by Manish.
           </Typography>
           <Typography variant="body2" component="p" align="center">
             Made with love by me..😁.
