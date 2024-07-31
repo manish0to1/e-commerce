9e65f18e16de20ee58ccd0215497416bb0485c8b
Manish Nayak <nayak10softworld@gmail.com>
2024-07-31 20:16:44 +0530

    creating filter data

---
+++ src/customer/components/Product/FilterData.js
@@ -0,0 +1,49 @@
+export const color = [
+  "White",
+  "Black",
+  "Red",
+  "Marun",
+  "Being",
+  "Pink",
+  "Yellow",
+  "Green",
+];
+
+export const filters = [
+  {
+    id: "color",
+    name: "Color",
+    options: [
+      { value: "white", label: "White" },
+      { value: "beige", label: "Beige" },
+      { value: "blue", label: "Blue" },
+      { value: "brown", label: "Brown" },
+      { value: "purple", label: "Purple" },
+      { value: "green", label: "Green" },
+      { value: "yellow", label: "Yellow" },
+    ],
+  },
+  {
+    id: "size",
+    name: "Size",
+    option: [
+      { value: "S", label: "S" },
+      { value: "M", label: "M" },
+      { value: "L", label: "L" },
+    ],
+  },
+];
+
+export const singleFilter = [
+  {
+    id: "price",
+    name: "Price",
+    option: [
+      { value: "₹159-399", label: "₹159 to ₹399" },
+      { value: "₹399-999", label: "₹399 to ₹999" },
+      { value: "₹999-1999", label: "₹999 to ₹1999" },
+      { value: "₹1999-2999", label: "₹1999 to ₹2999" },
+      { value: "₹3999-4999", label: "₹3999 to ₹4999" },
+    ],
+  },
+];
