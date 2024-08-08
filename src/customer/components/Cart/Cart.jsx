0e3a48fbbad803a932dadbeec19677250c61232f
Manish Nayak <nayak10softworld@gmail.com>
2024-08-08 19:45:45 +0530

    now creating add to cart button in cart section cart price details like price , discount , delivery charge etc.

--- src/customer/components/Cart/Cart.jsx
+++ src/customer/components/Cart/Cart.jsx
@@ -5,10 +5,43 @@ const Cart = () => {
   return (
     <div>
       <div className="lg:grid grid-cols-3 lg:px-16 relative">
-        <CartItem />
-      </div>
-      <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
-        <div className="border ">right side</div>
+        <div className="col-span-2">
+          <CartItem />
+        </div>
+        <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
+          <div className="border ">
+            <p className="font-bold opacity-60 pb-4">Price Details</p>
+            <hr />
+
+            <div className="space-y-3 font-semibold">
+              <div className="flex justify-between text-black pt-4">
+                <span>Price</span>
+                <span>₹4697</span>
+              </div>
+
+              <div className="flex justify-between text-black pt-4">
+                <span>Discount</span>
+                <span className="text-green-500">-₹3419</span>
+              </div>
+
+              <div className="flex justify-between text-black pt-4">
+                <span>Delivery Charge</span>
+                <span className="text-green-500">Free</span>
+              </div>
+
+              <div className="flex justify-between text-black pt-4 font-bold">
+                <span>Total Amount</span>
+                <span className="text-green-500">₹1278</span>
+              </div>
+            </div>
+            <button
+              variant="contained"
+              className="px-2.5rem py-0.5rem flex mt-8 justify-center items-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
