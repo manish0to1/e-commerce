7c769913c088224e532ff0a890a7c57305c979d3
Manish Nayak <nayak10softworld@gmail.com>
2024-08-10 11:25:39 +0530

    correcting the original cade with help of internet and fix the issue

--- src/customer/components/Checkout/DeliveryForm.jsx
+++ src/customer/components/Checkout/DeliveryForm.jsx
@@ -7,7 +7,6 @@ const DeliveryForm = () => {
     <div>
       <Grid container spacing={4}>
         <Grid
-          item
           xs={12}
           lg={5}
           className="border rounded-e-md shadow-md h-[30.5rem] overflow-y-scroll"
@@ -23,7 +22,7 @@ const DeliveryForm = () => {
                 textTransform: "none",
               }}
               size="large"
-              variant="contained"
+              variant="container"
             >
               Delivery Here
             </Button>
@@ -65,65 +64,3 @@ const DeliveryForm = () => {
 };
 
 export default DeliveryForm;
-
-// ######################### Original #################################
-// import { Box, Button, Grid, TextField } from "@mui/material";
-// import React from "react";
-// import AddressCard from "../AdderessCard/AddressCard";
-
-// const DeliveryForm = () => {
-//   return (
-//     <div>
-//       <Grid container spacing={4}>
-//         <Grid xs={12} lg={5} className="border rounded-e-md shadow-md h-[30.5rem] overflow-y-scroll">
-//           <div className="p-5 py-7 cursor-pointer">
-//             <AddressCard />
-//             <Button
-//               sx={{
-//                 mt: 2,
-//                 bgcolor: "RGB(145 85 253)",
-//                 color: "white",
-//                 "&:hover": { color: "black" },
-//                 textTransform: "none",
-//               }}
-//               size="large"
-//               variant="container"
-//             >
-//               Delivery Here
-//             </Button>
-//           </div>
-//         </Grid>
-
-//         <Grid item xs={12} lg={7}>
-//           <Box className="border rounded-s-md shadow-md p-5">
-//             <form>
-//               <Grid container xs={12} sm={6}>
-//                 <TextField
-//                   required
-//                   id="firstName"
-//                   name="firstName"
-//                   label="First Name"
-//                   fullWidth
-//                   autoComplete="given-name"
-//                 />
-//               </Grid>
-
-//               <Grid container xs={12} sm={6}>
-//                 <TextField
-//                   required
-//                   id="firstName"
-//                   name="firstName"
-//                   label="First Name"
-//                   fullWidth
-//                   autoComplete="given-name"
-//                 />
-//               </Grid>
-//             </form>
-//           </Box>
-//         </Grid>
-//       </Grid>
-//     </div>
-//   );
-// };
-
-// export default DeliveryForm;
