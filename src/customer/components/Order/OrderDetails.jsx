cc9645627b94e1facfdd9e53eac3cd40b8df9d07
Manish Nayak <nayak10softworld@gmail.com>
2024-08-12 23:49:55 +0530

    created order tracking stepper

--- src/customer/components/Order/OrderDetails.jsx
+++ src/customer/components/Order/OrderDetails.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import AddressCard from "../AdderessCard/AddressCard";
+import OrderTracking from "./OrderTracking";
 
 const OrderDetails = () => {
   return (
     <div className="px:5 lg:px-20">
-      <h1 className="font-bold text-xl py-5">delivery Address</h1>
-      <AddressCard />
+      <div>
+        <h1 className="font-bold text-xl py-5">delivery Address</h1>
+        <AddressCard />
+      </div>
+      <div>
+        <OrderTracking activeStep={3} />
+      </div>
     </div>
   );
 };
--- src/customer/components/Order/OrderTracking.jsx
+++ src/customer/components/Order/OrderTracking.jsx
@@ -1,7 +1,28 @@
+import { Step, StepLabel, Stepper } from "@mui/material";
 import React from "react";
 
-const OrderTracking = () => {
-  return <div>Order Tracking Page !</div>;
+const steps = [
+  "Order Confirmed",
+  "Placed",
+  "Shipped",
+  "Ou for Delivery",
+  "Delivered",
+];
+
+const OrderTracking = ({ activeStep }) => {
+  return (
+    <div className="w-full">
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step>
+            <StepLabel sx={{ color: "#9155FD", font: "40px" }}>
+              {label}
+            </StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
+  );
 };
 
 export default OrderTracking;
