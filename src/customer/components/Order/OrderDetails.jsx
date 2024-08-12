a03f0bc1ca3b736b883574b28ab30aab330dfefb
Manish Nayak <nayak10softworld@gmail.com>
2024-08-13 00:06:15 +0530

    working on order details like title , size , seller etc

--- src/customer/components/Order/OrderDetails.jsx
+++ src/customer/components/Order/OrderDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AddressCard from "../AdderessCard/AddressCard";
 import OrderTracking from "./OrderTracking";
+import { Grid } from "@mui/material";
 
 const OrderDetails = () => {
   return (
@@ -9,9 +10,33 @@ const OrderDetails = () => {
         <h1 className="font-bold text-xl py-5">delivery Address</h1>
         <AddressCard />
       </div>
-      <div>
+      <div className="py-3">
         <OrderTracking activeStep={3} />
       </div>
+
+      <Grid className="space-x-5" container>
+        <Grid
+          item
+          container
+          className="shadow-xl rounded-md p-5 border"
+          sx={{ alignContent: "center", justifyContent: "space-between" }}
+        >
+          <Grid item xs={6}>
+            <div className="flex space-x-5">
+              <img
+                className="w-[5rem] h-[5rem] object-cover object-top"
+                src="https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/g/6/k/m-sksh-dt1105-pcbl-fubar-original-imafux247zhqym2z-bb.jpeg?q=70"
+                alt=""
+              />
+              <div>
+                <p>title</p>
+                <p>size</p>
+                <p>seller</p>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
