8f54bdf3dbfc37800d37b0c7adfaa72fa88c78b4
Manish Nayak <nayak10softworld@gmail.com>
2024-08-13 23:57:20 +0530

    great ! few hours after i founf a new error

--- src/customer/components/Order/OrderDetails.jsx
+++ src/customer/components/Order/OrderDetails.jsx
@@ -1,48 +1,51 @@
 import React from "react";
 import AddressCard from "../AdderessCard/AddressCard";
 import OrderTracking from "./OrderTracking";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 
 const OrderDetails = () => {
   return (
-    <div className="px:5 lg:px-20">
+    <div className="px-5 lg:px-20">
       <div>
-        <h1 className="font-bold text-xl py-5">delivery Address</h1>
+        <h1 className="font-bold text-xl py-5">Delivery Address</h1>
         <AddressCard />
       </div>
       <div className="py-3">
         <OrderTracking activeStep={3} />
       </div>
 
-      <Grid className="space-x-5" container>
+      <Grid container spacing={2} className="py-5">
         <Grid
           item
           container
           className="shadow-xl rounded-md p-5 border"
-          sx={{ alignContent: "center", justifyContent: "space-between" }}
+          sx={{ alignItems: "center", justifyContent: "space-between" }}
         >
           <Grid item xs={6}>
-            <div>
+            <div className="flex items-center space-x-5">
               <img
                 className="w-[5rem] h-[5rem] object-cover object-top"
                 src="https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/g/6/k/m-sksh-dt1105-pcbl-fubar-original-imafux247zhqym2z-bb.jpeg?q=70"
                 alt=""
               />
               <div className="space-y-2 ml-5">
-                <p>title</p>
+                <p>Title</p>
                 <p className="space-x-5">
-                  <span>Size : M </span>
+                  <span>Size : M</span>
                   <span>Color : color</span>
                 </p>
-                <p>eller</p>
+                <p>Seller</p>
                 <p>Price</p>
               </div>
             </div>
           </Grid>
-{/* ################## Rating Grid ################# */}
+          {/* ################## Rating Grid ################# */}
           <Grid item>
-
-
+            <Box sx={{ color: "blue" }}>
+              <StarBorderIcon sx={{ fontSize: "2rem" }} className="px-2" />
+              <span>Rate & Reviews</span>
+            </Box>
           </Grid>
         </Grid>
       </Grid>
@@ -51,3 +54,62 @@ const OrderDetails = () => {
 };
 
 export default OrderDetails;
+
+// import React from "react";
+// import AddressCard from "../AdderessCard/AddressCard";
+// import OrderTracking from "./OrderTracking";
+// import { Box, Grid } from "@mui/material";
+// import StarIcon from "@mui/icons-material/Star";
+
+// const OrderDetails = () => {
+//   return (
+//     <div className="px:5 lg:px-20">
+//       <div>
+//         <h1 className="font-bold text-xl py-5">delivery Address</h1>
+//         <AddressCard />
+//       </div>
+//       <div className="py-3">
+//         <OrderTracking activeStep={3} />
+//       </div>
+
+//       <Grid className="space-x-5" container>
+//         <Grid
+//           item
+//           container
+//           className="shadow-xl rounded-md p-5 border"
+//           sx={{ alignContent: "center", justifyContent: "space-between" }}
+//         >
+//           <Grid item xs={6}>
+//             <div>
+//               <img
+//                 className="w-[5rem] h-[5rem] object-cover object-top"
+//                 src="https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/g/6/k/m-sksh-dt1105-pcbl-fubar-original-imafux247zhqym2z-bb.jpeg?q=70"
+//                 alt=""
+//               />
+//               <div className="space-y-2 ml-5">
+//                 <p>title</p>
+//                 <p className="space-x-5">
+//                   <span>Size : M </span>
+//                   <span>Color : color</span>
+//                 </p>
+//                 <p>Seller</p>
+//                 <p>Price</p>
+//               </div>
+//             </div>
+//           </Grid>
+
+//           {/* ################## Rating Grid ################# */}
+//           <Grid item>
+
+//             <Box sx={{ color: "blue" }}>
+//               <StarIcon fontSize={2} className="px-2 text-5xl " />
+//               <span>Rate & Reviews</span>
+//             </Box>
+//           </Grid>
+//         </Grid>
+//       </Grid>
+//     </div>
+//   );
+// };
+
+// export default OrderDetails;
