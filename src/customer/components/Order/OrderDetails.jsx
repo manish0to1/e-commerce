a6fef99f9dc402bcb6c586f3bfbac362042c2075
Manish Nayak <nayak10softworld@gmail.com>
2024-08-14 23:17:33 +0530

    creating multiple orderDetails or hardCode for testing the ui

--- src/customer/components/Order/OrderDetails.jsx
+++ src/customer/components/Order/OrderDetails.jsx
@@ -11,43 +11,46 @@ const OrderDetails = () => {
         <h1 className="font-bold text-xl py-5">Delivery Address</h1>
         <AddressCard />
       </div>
+
       <div className="py-3">
         <OrderTracking activeStep={3} />
       </div>
 
-      <Grid container spacing={2} className="py-5">
-        <Grid
-          item
-          container
-          className="shadow-xl rounded-md p-5 border"
-          sx={{ alignItems: "center", justifyContent: "space-between" }}
-        >
-          <Grid item xs={6}>
-            <div className="flex items-center space-x-5">
-              <img
-                className="w-[5rem] h-[5rem] object-cover object-top"
-                src="https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/g/6/k/m-sksh-dt1105-pcbl-fubar-original-imafux247zhqym2z-bb.jpeg?q=70"
-                alt=""
-              />
-              <div className="space-y-2 ml-5">
-                <p>Title</p>
-                <p className="space-x-5">
-                  <span>Size : M</span>
-                  <span>Color : color</span>
-                </p>
-                <p>Seller</p>
-                <p>Price</p>
+      <Grid container spacing={2} className="space-y-5">
+        {[1, 1, 1, 1].map((item) => (
+          <Grid
+            item
+            container
+            className="shadow-xl rounded-md p-5 border"
+            sx={{ alignItems: "center", justifyContent: "space-between" }}
+          >
+            <Grid item xs={6}>
+              <div className="flex items-center space-x-5">
+                <img
+                  className="w-[5rem] h-[5rem] object-cover object-top"
+                  src="https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/g/6/k/m-sksh-dt1105-pcbl-fubar-original-imafux247zhqym2z-bb.jpeg?q=70"
+                  alt=""
+                />
+                <div className="space-y-2 ml-5">
+                  <p className="font-semibold">Title</p>
+                  <p className="space-x-5 text-xs font-semibold opacity-50">
+                    <span>Size : M</span>
+                    <span>Color : color</span>
+                  </p>
+                  <p>Seller</p>
+                  <p>Price</p>
+                </div>
               </div>
-            </div>
-          </Grid> 
-          {/* ################## Rating Grid ################# */}
-          <Grid item>
-            <Box sx={{ color: "blue" }}>
-              <StarBorderIcon sx={{ fontSize: "2rem" }} className="px-2" />
-              <span>Rate & Reviews</span>
-            </Box>
+            </Grid>
+            {/* ################## Rating Grid ################# */}
+            <Grid item>
+              <Box sx={{ color: "blue" }}>
+                <StarBorderIcon sx={{ fontSize: "2rem" }} className="px-2" />
+                <span>Rate & Reviews</span>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        ))}
       </Grid>
     </div>
   );
