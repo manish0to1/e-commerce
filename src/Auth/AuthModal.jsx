cdf71eeadf62a39d61f271fa1441c7ff2cbb2348
Manish Nayak <nayak10softworld@gmail.com>
2024-10-07 21:52:01 +0530

    working on modal in authModal

--- src/Auth/AuthModal.jsx
+++ src/Auth/AuthModal.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const AuthModel = () => {
   return (
     <div>
-      
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default AuthModel
+export default AuthModel;
