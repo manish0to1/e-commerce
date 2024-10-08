210913ae0978c587e0bc5c5111c3961cacc1971e
Manish Nayak <nayak10softworld@gmail.com>
2024-10-08 14:04:24 +0530

    creating register fields name , email etc

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -1,7 +1,48 @@
+import { Grid, TextField } from "@mui/material";
 import React from "react";
 
 const Register = () => {
-  return <div># Register Form Component</div>;
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      # Register Form Component
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
