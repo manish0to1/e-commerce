3838c3580e8a9612a33610be664660e91133adac
Manish Nayak <nayak10softworld@gmail.com>
2024-10-08 13:06:55 +0530

    working on authModal

--- src/Auth/AuthModal.jsx
+++ src/Auth/AuthModal.jsx
@@ -1,6 +1,19 @@
+import { Box, Modal, Typography } from "@mui/material";
 import React from "react";
 
-const AuthModel = () => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const AuthModel = ({ handleClose, open }) => {
   return (
     <div>
       <Modal
