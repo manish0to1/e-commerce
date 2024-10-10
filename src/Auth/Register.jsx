f8d33c0b19ca2a4368981891b661b4b77b471a62
Manish Nayak <nayak10softworld@gmail.com>
2024-10-10 16:34:19 +0530

    working on registerPage

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -10,26 +10,17 @@ const Register = () => {
       # Register Form Component
       <form onSubmit={handleSubmit}>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12}>
             <TextField
               required
-              id="firstName"
-              name="firstName"
-              label="First Name"
+              id="name"
+              name="name"
+              label="Name here"
               fullWidth
               autoComplete="given-name"
             />
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="lastName"
-              name="lastName"
-              label="Last Name"
-              fullWidth
-              autoComplete="given-name"
-            />
-          </Grid>
+
           <Grid item xs={12}>
             <TextField
               required
