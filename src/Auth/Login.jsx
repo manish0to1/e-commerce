c2a5c7906b46387c3833259c82d1d80133be3377
Manish Nayak <nayak10softworld@gmail.com>
2024-10-12 18:32:33 +0530

    creating login form

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="name"
+              name="name"
+              label="Name"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email"
+              fullWidth
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="password"
+              name="password"
+              label="Password"
+              fullWidth
+              autoComplete="password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              className="bg-[#9155FD] w-full"
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ padding: ".8rem 0", bgcolor: "#9155FD" }}
+            >
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
