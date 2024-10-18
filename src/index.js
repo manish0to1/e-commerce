1a0fb301a21400f4dd0f40d8bfb204f066cb7098
Manish Nayak <nayak10softworld@gmail.com>
2024-10-18 10:16:23 +0530

    working on states or redux

--- src/index.js
+++ src/index.js
@@ -24,12 +24,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
