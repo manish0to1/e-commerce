6d9dc36360f029462f9e9d19029271bd0028bfdb
Manish Nayak <nayak10softworld@gmail.com>
2024-10-19 00:22:16 +0530

    working on state's auth's action

--- src/State/Auth/Action.js
+++ src/State/Auth/Action.js
@@ -5,6 +5,9 @@ const {
   REGISTER_REQUEST,
   REGISTER_SUCCESS,
   REGISTER_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
 } = require("./ActionType");
 
 const registerRequest = () => ({ type: REGISTER_REQUEST });
@@ -25,16 +28,20 @@ export const register = (userData) => async (dispatch) => {
   }
 };
 
+const loginRequest = () => ({ type: LOGIN_REQUEST });
+const loginSuccess = (user) => ({ type: LOGIN_SUCCESS, payload: user });
+const loginFailure = (error) => ({ type: LOGIN_FAILURE, payload: error });
+
 export const login = (userData) => async (dispatch) => {
-  dispatch(registerRequest());
+  dispatch(loginRequest());
   try {
     const response = await axios.post(`${API_BASE_URL}/auth/signin`, userData);
     const user = response.data;
     if (user.jwt) {
       localStorage.setItem("jwt", user.jwt);
     }
-    dispatch(registerSuccess());
+    dispatch(loginSuccess());
   } catch (error) {
-    dispatch(registerFailure(error.message));
+    dispatch(loginFailure(error.message));
   }
 };
