319b7d4466bd213086fb51bf934f16e39c738c7f
Manish Nayak <nayak10softworld@gmail.com>
2024-10-18 19:50:29 +0530

    working on state's action

--- src/State/Auth/Action.js
+++ src/State/Auth/Action.js
@@ -1,9 +1,23 @@
+const { default: axios } = require("axios");
+const { API_BASE_URL } = require("../../config/apiConfig");
 
-const register=(userData)=>(dispatch)=>{
+const {
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+} = require("./ActionType");
 
-    try {
-        
-    } catch (error) {
-        
+const registerRequest = () => ({ type: REGISTER_REQUEST });
+const registerSuccess = (user) => ({ type: REGISTER_SUCCESS, payload: user });
+const registerFailure = (error) => ({ type: REGISTER_FAILURE, payload: error });
+
+const register = (userData) => async (dispatch) => {
+  dispatch(registerRequest());
+  try {
+    const response = await axios.post(`${API_BASE_URL}/auth/signup`, userData);
+    const user = response.data;
+    if (user.jwt) {
+      localStorage.setItem("jwt", user.jwt);
     }
-}
+  } catch (error) {}
+};
