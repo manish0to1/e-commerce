5e2f8b2eb10dfdfdb444721500ff12f01933dfe0
Manish Nayak <nayak10softworld@gmail.com>
2024-10-19 20:53:16 +0530

    working on auth's state's ReducerFile

--- src/State/Auth/Reducer.js
+++ src/State/Auth/Reducer.js
@@ -1,7 +1,10 @@
 import {
   GET_USER_REQUEST,
+  GET_USER_SUCCESS,
   LOGIN_REQUEST,
+  LOGIN_SUCCESS,
   REGISTER_REQUEST,
+  REGISTER_SUCCESS,
 } from "./ActionType";
 
 const initialState = {
@@ -16,6 +19,11 @@ export const authReducer = (state = initialState, action) => {
     case LOGIN_REQUEST:
     case GET_USER_REQUEST:
       return { ...state, isLoading: true, error: null };
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      return { ...state, isLoading: false, error: null, jwt: action.payload };
+    case GET_USER_SUCCESS:
+      return { ...state, isLoading: false, error: null, user: action.payload };
     default:
       return state;
   }
