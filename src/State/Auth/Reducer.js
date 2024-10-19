16db05e373b2f1212398c8301455082e13060361
Manish Nayak <nayak10softworld@gmail.com>
2024-10-19 21:00:28 +0530

    working on reducer's authReducer

--- src/State/Auth/Reducer.js
+++ src/State/Auth/Reducer.js
@@ -1,8 +1,12 @@
 import {
+  GET_USER_FAILURE,
   GET_USER_REQUEST,
   GET_USER_SUCCESS,
+  LOGIN_FAILURE,
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
+  LOGOUT,
+  REGISTER_FAILURE,
   REGISTER_REQUEST,
   REGISTER_SUCCESS,
 } from "./ActionType";
@@ -24,6 +28,12 @@ export const authReducer = (state = initialState, action) => {
       return { ...state, isLoading: false, error: null, jwt: action.payload };
     case GET_USER_SUCCESS:
       return { ...state, isLoading: false, error: null, user: action.payload };
+    case REGISTER_FAILURE:
+    case LOGIN_FAILURE:
+    case GET_USER_FAILURE:
+      return { ...state, isLoading: false, error: action.payload };
+    case LOGOUT:
+      return { ...initialState };
     default:
       return state;
   }
