a462c88cff3c99917897b38c2eb8fdd54f38b3cd
Manish Nayak <nayak10softworld@gmail.com>
2024-10-23 23:37:45 +0530

    working navigation

--- src/customer/components/Navigation/Navigation.jsx
+++ src/customer/components/Navigation/Navigation.jsx
@@ -437,7 +437,7 @@ export default function Navigation() {
                       onClick={handleOpen}
                       className="text-sm font-medium text-gray-700 hover:text-gray-800"
                     >
-                      Sign in
+                      Sign In
                     </Button>
                   )}
                 </div>
